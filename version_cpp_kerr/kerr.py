3e33bb7c8e04e201a64ccf6e23ed931d62df15b9
Marie_Clementine_Quilleriet <quilleriet.marieclementine@gmail.com>
2020-06-25 10:57:22 +0200

    kerr dispo pour interface.py

--- version_cpp_kerr/kerr.py
+++ version_cpp_kerr/kerr.py
@@ -1,3 +1,9 @@
 import kerr
-
+'''import matplotlib.image as mpimg
+import numpy as np
+img = mpimg.imread("adisk_upscaled.png")
+import matplotlib.pyplot as plt
+plt.imshow(img)
+plt.show()'''
 kerr.image()
+#kerr.image("params.txt")
